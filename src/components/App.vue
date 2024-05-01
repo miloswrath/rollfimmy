44bcd99fbe7aa4b986b344e7cfe8327279e92022
zjgilliam <zachary-gilliam@uiowa.edu>
2024-04-30 22:32:41 -0500

    aksdjhkasjdh

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,4 +1,3 @@
-
 <script>
 import axios from "axios";
 import TimelineComponent from "/src/components/TimelineComponent.vue";
@@ -20,6 +19,7 @@ export default {
       filterYearRange: [null, null],
       filterSearch: "",
       filterGenres: [],
+      clearBool: false,
     };
   },
   mounted() {
@@ -46,42 +46,65 @@ export default {
       this.filterDirectors = criteria.filterDirectors;
       this.filterStars = criteria.filterStars;
       this.filterSearch = criteria.filterSearch;
+      this.clearBool = criteria.clearBool;
       console.log("Received filter update:", criteria);
       this.filteredDataComputed();
     },
+    clearData() {
+      this.filteredData = initialData;
+    },
     filteredDataComputed() {
-        let result = this.filteredData;
-        console.log("Year range", this.filterYearRange);
-        console.log("Genres", this.filterGenres);
-        // Filter by year range
-        if (this.filterYearRange[0] && this.filterYearRange[1]) {
-          result = result.filter(
-            (item) =>
-              item.Released_Year >= this.filterYearRange[0] &&
-              item.Released_Year <= this.filterYearRange[1]
-          );
-        }
+      let result = this.filteredData;
+      console.log("Year range", this.filterYearRange);
+      console.log("Genres", this.filterGenres);
+      console.log("Search", this.filterSearch);
+      console.log("clearBool", this.clearBool);
+      if (this.clearBool) {
+        this.filteredData = this.initialData;
+        console.log(
+          "filteredData set in App.vue within clear:",
+          this.filteredData
+        );
+        this.clearBool = false;
+        return this.filteredData;
+      }
+      // Filter by year range
 
-        // Filter by genres
-        if (this.filterGenres.length > 0) {
-          result = result.filter((item) =>
-            this.filterGenres.some((genre) =>
-              item.Genre.toLowerCase().includes(genre.toLowerCase())
-            )
-          );
-        }
+      if (this.filterYearRange[0] && this.filterYearRange[1]) {
+        result = result.filter(
+          (item) =>
+            item.Released_Year >= this.filterYearRange[0] &&
+            item.Released_Year <= this.filterYearRange[1]
+        );
+      }
+      if (this.filterSearch) {
+        result = result.filter((item) =>
+          item.Series_Title.toLowerCase().includes(
+            this.filterSearch.toLowerCase()
+          )
+        );
+      }
+      // Filter by genres
+      if (this.filterGenres.length > 0) {
+        result = result.filter((item) =>
+          this.filterGenres.some((genre) =>
+            item.Genre.toLowerCase().includes(genre.toLowerCase())
+          )
+        );
+      }
 
       this.filteredData = result;
       console.log("result from filteredDataComputed in App.vue:", result);
       console.log("filteredData set in App.vue:", this.filteredData);
       return this.filteredData;
-      },
     },
+
     watch: {
       filteredData(newVal, oldVal) {
         console.log("App.vue filteredData changed:", newVal);
       },
     },
+  },
 };
 </script>
 
@@ -104,7 +127,7 @@ export default {
 }
 
 .v-timeline__image img {
-  width:95%;
+  width: 95%;
   /* Ensures the image takes up 75% oih the full width of its container */
   height: auto;
   /* Keeps the aspect ratio of the image */
@@ -117,13 +140,28 @@ export default {
   padding-left: var(--vq4-space-md);
   /* Adds some space between the image and text */
 }
+header {
+  display: flex;
+  align-items: center; /* This vertically centers the logo if the header is taller than the logo */
+  padding: 10px; /* Add padding around the content of the header */
+}
+
+.logo {
+  height: 50px; /* Or your desired size */
+  width: auto; /* Maintain aspect ratio */
+  margin-right: 20px; /* Space between logo and any other header content */
+}
 </style>
 <template>
+  <header>
+    <img src="./src/assets/looo.svg" alt="Logo" class="logo" />
+    <!-- Rest of your header or nav content -->
+  </header>
   <div
     class="vq4-position-relative vq4-z-index-1 vq4-overflow-hidden vq4-padding-y-xl vq4-bg-dark"
   >
     <div class="vq4-margin-bottom-lg">
-      <h1 class="vq4-text-center">Vertical Timeline</h1>
+      <h1 class="vq4-text-center">RollFilm</h1>
     </div>
     <div>
       <filter-component
@@ -131,7 +169,10 @@ export default {
         :initial-data="filteredData"
       ></filter-component>
 
-      <timeline-component v-if="filteredData && filteredData.length" :filtered-data="filteredData"></timeline-component>
+      <timeline-component
+        v-if="filteredData && filteredData.length"
+        :filtered-data="filteredData"
+      ></timeline-component>
     </div>
   </div>
 </template>
--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,22 +1,69 @@
 <template>
   <div>
     <button @click="toggleFilters">Filters</button>
+    <div class="search-input search-input--icon-right">
+      <input
+        class="search-input__input form-control"
+        type="search"
+        name="search-input"
+        id="search-input"
+        placeholder="Search..."
+        aria-label="Search"
+        v-model="filterSearch"
+      />
+      <button class="search-input__btn" @click="applyFilters">
+        <svg class="icon" viewBox="0 0 20 20">
+          <title>Submit</title>
+
+          <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <circle cx="8" cy="8" r="6" />
+            <line x1="12.242" y1="12.242" x2="18" y2="18" />
+          </g>
+        </svg>
+      </button>
+    </div>
     <div v-if="showFilters">
-      <!-- Filter by Year -->
+      <div class="slider slider--multi-value js-slider">
+        <div class="slider__range">
+          <label class="sr-only" for="sliderMinValue"> </label>
+          <input
+            class="slider__input"
+            type="range"
+            id="sliderMinValue"
+            name="sliderMinValue"
+            min="1920"
+            max="2020"
+            step="1"
+            value="100"
+            v-model="filterYearRange[0]"
+          />
+        </div>
+
+        <div class="slider__range">
+          <label class="sr-only" for="sliderMaxValue"> </label>
+          <input
+            class="slider__input"
+            type="range"
+            id="sliderMaxValue"
+            name="sliderMaxValue"
+            min="1920"
+            max="2020"
+            step="1"
+            value="100"
+            s
+            v-model="filterYearRange[1]"
+          />
+        </div>
+        <!-- Filter by Year -->
+      </div>
       <div>
         <label>Year: {{ filterYearRange[0] }} - {{ filterYearRange[1] }}</label>
-        <input
-          type="range"
-          min="1920"
-          max="1970"
-          v-model.number="filterYearRange[0]"
-        />
-        <input
-          type="range"
-          min="1971"
-          max="2020"
-          v-model.number="filterYearRange[1]"
-        />
       </div>
 
       <!-- Filter by Genre -->
@@ -26,28 +73,46 @@
           <input type="checkbox" :value="genre" v-model="filterGenres" />
           {{ genre }}
         </div>
+        <!-- Apply Filters Button -->
+        <button @click="applyFilters">Apply Filters</button>
       </div>
-
-      <!-- Apply Filters Button -->
-      <button @click="applyFilters">Apply Filters</button>
+      <button @click="clearFilters">Clear Filters</button>
     </div>
   </div>
 </template>
 
 <script>
+import Util from "/src/Util.js";
+
 export default {
-  name: 'FilterComponent',
+  name: "FilterComponent",
   data() {
     return {
       showFilters: false,
       filterGenres: [],
       filterYearRange: [1920, 2020],
       genres: ["Action", "Drama", "Comedy", "Thriller"],
+      clearBool: false,
     };
   },
   methods: {
     toggleFilters() {
       this.showFilters = !this.showFilters;
+    },
+    clearFilters() {
+      this.filterGenres = [];
+      this.filterYearRange = [1920, 2020];
+      this.filterDirectors = [];
+      this.filterStars = [];
+      this.filterSearch = "";
+      this.clearBool = true;
+      console.log("FilterComponent clearFilters clearBool:", {
+        filterGenres: this.filterGenres,
+        filterYearRange: this.filterYearRange,
+        clearBool: this.clearBool,
+      });
+      this.applyFilters();
+      this.clearBool = false;
     },
     applyFilters() {
       // Emit all filter criteria to the parent component
@@ -62,8 +127,99 @@ export default {
         filterDirectors: this.filterDirectors,
         filterStars: this.filterStars,
         filterSearch: this.filterSearch,
+        clearBool: this.clearBool,
       });
     },
   },
 };
 </script>
+
+<style scoped>
+.slider--multi-value {
+  position: relative;
+}
+.slider--multi-value .slider__range {
+  pointer-events: none;
+}
+
+.slider--multi-value .slider__range:nth-of-type(1) {
+  --slider-fill-value-start: 0%;
+  --slider-fill-value-end: 100%;
+}
+
+.slider--multi-value .slider__range:nth-of-type(2) {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.slider--multi-value .slider__input::-webkit-slider-thumb {
+  position: relative;
+  z-index: 2;
+  pointer-events: auto;
+}
+.slider--multi-value .slider__input::-moz-range-thumb {
+  position: relative;
+  z-index: 2;
+  pointer-events: auto;
+}
+.slider--multi-value .slider__input::-webkit-slider-runnable-track {
+  pointer-events: none;
+  background-image: linear-gradient(
+    to right,
+    var(--color-contrast-lower) var(--slider-fill-value-start),
+    var(--color-primary) var(--slider-fill-value-start),
+    var(--color-primary) var(--slider-fill-value-end),
+    var(--color-contrast-lower) var(--slider-fill-value-end)
+  );
+}
+.slider--multi-value .slider__input::-moz-range-track {
+  pointer-events: none;
+  background-image: linear-gradient(
+    to right,
+    var(--color-contrast-lower) var(--slider-fill-value-start),
+    var(--color-primary) var(--slider-fill-value-start),
+    var(--color-primary) var(--slider-fill-value-end),
+    var(--color-contrast-lower) var(--slider-fill-value-end)
+  );
+}
+.slider--multi-value
+  .slider__range:nth-of-type(2)
+  .slider__input::-moz-range-track {
+  background-image: none;
+}
+
+:not(*)::-ms-track,
+.slider--multi-value.slider--ms-fallback {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+:not(*)::-ms-track,
+.slider--multi-value.slider--ms-fallback .slider__range {
+  pointer-events: auto;
+  width: 48%;
+}
+
+:not(*)::-ms-track,
+.slider--multi-value.slider--ms-fallback .slider__range:nth-of-type(2) {
+  position: relative;
+}
+
+.slider__separator {
+  margin: auto var(--space-xs);
+}
+
+.slider--multi-value.slider--range-not-supported .slider__range {
+  display: inline-block;
+  width: 45%;
+}
+.slider--multi-value.slider--range-not-supported .slider__range:nth-of-type(2) {
+  position: relative;
+}
+
+.slider--range-not-supported .slider__separator {
+  display: none;
+}
+</style>
